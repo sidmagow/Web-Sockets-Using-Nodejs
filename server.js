5b9bcb14b9aab2053e49e55a72735fd3783cdb7d
sidmagow <siddhant.magow@gmail.com>
2017-03-08 00:17:57 +0530

    chat

---
+++ server.js
@@ -0,0 +1,50 @@
+/**
+ * Created by championswimmer on 18/02/17.
+ */
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const socket = require('socket.io');
+
+const io = socket(server);
+
+let chats = [];
+let clients = {};
+
+io.on("connection", function(conn) {
+    console.log("A client has connected" + conn.id);
+
+
+    conn.emit("chatlog", chats);//not possible earlier as client was not able to listen but with sockets it is possible
+
+    conn.on("signup", function (data) {
+        clients[data] = conn.id;
+        console.log(clients);
+    });
+
+    conn.on("send_msg", function (data) {
+        if (data.msg.charAt(0) == "@") {
+            let toUser = data.msg.split(" ", 1)[0].substring(1);
+            let msg = data.msg.substring(data.msg.indexOf(" ") + 1);
+
+            io.to(clients[toUser]).emit("rcv_msg", data.user + ": " + msg);
+        } else {
+            chats.push(data);
+            io.emit("rcv_msg", data.user + ": " + data.msg)
+        }
+    });
+
+    conn.on("disconnect", function (data) {
+        console.log("A user disconnected");
+    })
+
+} );
+
+
+app.use('/', express.static(__dirname + "/public"));
+
+
+server.listen(3456, function () {
+    console.log('Server started');
+});
